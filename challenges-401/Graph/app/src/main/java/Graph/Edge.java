3743673f0f35e3126534601a6f2d18d5e74be05e
obada-naser <obadanaser135@gmail.com>
2021-11-25 23:31:32 +0200

    adding nodes

---
+++ challenges-401/Graph/app/src/main/java/Graph/Edge.java
@@ -0,0 +1,12 @@
+package Graph;
+
+public class Edge {
+    int src;
+    int dest;
+    int weight;
+    Edge(int src, int dest, int weight) {
+        this.src = src;
+        this.dest = dest;
+        this.weight = weight;
+    }
+}
